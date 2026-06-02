167041874bc89f0fc62f47b31af7ad052777c871
AXEL-HG <hernandezgonzalezkevinaxel@gmail.com>
2026-06-01 20:30:34 -0600

    Add Persona class with attributes and getters/setters

---
+++ Practicas/practica_03_sistema_de_control_universitario/src/core/Persona.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Persona {
+    // Atributos limpitos y en minúsculas
+    private String nombre;
+    private String direccion;
+    private LocalDate fechaNacimiento;
+
+    //Metodos
+
+    public void actualizarDireccion(String nuevaDireccion) {
+        this.direccion = nuevaDireccion;
+    }
+
+    public int obtenerEdad() {
+        if (this.fechaNacimiento == null) return 0;
+        return Period.between(this.fechaNacimiento, LocalDate.now()).getYears();
+    }
+
+    public boolean esAdultoMayor() {
+        return obtenerEdad() >= 60;
+    }
+
+    @Override
+    public String toString(){
+        return "Persona: " + nombre + ". Edad: " + obtenerEdad();
+    }
+
+
+    // --- GETTERS & SETTERS ---
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getDireccion() { return direccion; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+
+    public LocalDate getFechaNacimiento() { return fechaNacimiento; }
+    public void setFechaNacimiento(LocalDate fechaNacimiento) { this.fechaNacimiento = fechaNacimiento; }
+}
